69f2c42bcfdb7d6afbed71729273b78e45707f90
lotje1990 <lotje1990@hotmail.com>
2015-06-29 15:31:59 +0200

    HQ controller

--- CIMS-Headquarters/src/main/java/HeadquartersApp/UI/HeadquartersController.java
+++ CIMS-Headquarters/src/main/java/HeadquartersApp/UI/HeadquartersController.java
@@ -186,6 +186,7 @@ public class HeadquartersController implements Initializable {
     @FXML ComboBox cbtNewExecutor;
     @FXML Label lblTasks;
     @FXML Button btnNewTask;
+    @FXML Button btnMarkRead;
 
     private IData requestData;
     private ISortedData sortedData;
@@ -1275,8 +1276,20 @@ public class HeadquartersController implements Initializable {
 
             @Override
             public void run() {
-                tvtTasks.getItems().removeAll(tasks);
-                tvtTasks.getItems().addAll(tasks);
+                for(ITask t : tasks){
+                    if(tvtTasks.getItems().contains(t)){
+                        tvtTasks.getItems().remove(t);
+                        System.out.println(tvtTasks.getItems().contains(t));
+                        if(t.getStatus() != TaskStatus.READ)
+                        {
+                            System.out.println(t.getStatus());
+                            tvtTasks.getItems().add(t);
+                        }
+                    }
+                    else{
+                        tvtTasks.getItems().add(t);
+                    }
+                }
                 if (tvtTasks.getSelectionModel().getSelectedItem() == null) {
                     tvtTasks.getSelectionModel().selectFirst();
                 }
@@ -1303,15 +1316,21 @@ public class HeadquartersController implements Initializable {
                 tftExecutor.setText(task.getExecutor().toString());
             }
 
-            if (task.getStatus() != TaskStatus.REFUSED && task.getStatus() != TaskStatus.FAILED) {
-
+            if (task.getStatus() == TaskStatus.REFUSED) {
+                cbtNewExecutor.setDisable(false);
+                btnNewTask.setDisable(false);
+                btnMarkRead.setDisable(true);
+                tftReason.setDisable(false);
+            }  else if(task.getStatus() == TaskStatus.SUCCEEDED  && task.getStatus() != TaskStatus.FAILED){
+                cbtNewExecutor.setDisable(true);
+                btnNewTask.setDisable(true);
+                btnMarkRead.setDisable(false);
+                tftReason.setDisable(true);
+            }else {
                 cbtNewExecutor.setDisable(true);
                 btnNewTask.setDisable(true);
                 tftReason.setDisable(true);
-            } else {
-                cbtNewExecutor.setDisable(false);
-                btnNewTask.setDisable(false);
-                tftReason.setDisable(false);
+                btnMarkRead.setDisable(true);
             }
 
             tftReason.setText(task.getDeclineReason());
